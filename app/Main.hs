7d1605f84bb106e66835f2411646be49ba36e894
Eden <edendelgado@gmail.com>
2020-05-28 08:57:18 -0500

    Added WAI package
    tried out the examples they have and got hello working

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,46 @@
-module Main where
+{-# LANGUAGE OverloadedStrings #-}
+import Network.Wai
+import Network.HTTP.Types
+import Network.Wai.Handler.Warp (run)
 
-import Lib
+app :: Application
+app _ respond = do
+    putStrLn "I've done some IO here"
+    respond $ responseLBS
+        status200
+        [("Content-Type", "text/plain")]
+        "Hello, Web!"
 
 main :: IO ()
-main = someFunc
+main = do
+    putStrLn $ "http://localhost:8080/"
+    run 8080 app3
+
+appTwo :: Application
+appTwo _ respond = respond index
+
+index :: Response
+index = responseFile
+    status200
+    [("Content-Type", "text/html")]
+    "index.html"
+    Nothing
+
+app3 :: Application
+app3 request respond = respond $
+  case rawPathInfo request of
+    "/"     -> index
+    "/raw/" -> plainIndex
+    _       -> notFound
+
+plainIndex :: Response
+plainIndex = responseLBS
+    status200
+    [("Content-Type", "text/plain")]
+    "Hello, PlainText"
+
+notFound :: Response
+notFound = responseLBS
+    status404
+    [("Content-Type", "text/plain")]
+    "404 - Not Found"
