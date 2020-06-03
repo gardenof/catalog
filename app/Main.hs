a2a6b7e1c1f4a081b1a9573576c7893a0e60b7c2
Eden <edendelgado@gmail.com>
2020-06-02 23:37:34 -0500

    add Blaze package
    got blaze test page working and got Wai to serve the page

--- app/Main.hs
+++ app/Main.hs
@@ -3,6 +3,14 @@ import Network.Wai
 import Network.HTTP.Types
 import Network.Wai.Handler.Warp (run)
 
+import Control.Monad (forM_)
+import qualified Text.Blaze.Html5 as H
+import qualified Text.Blaze.Html5.Attributes as A
+import qualified Text.Blaze.Html.Renderer.Utf8 as BHRU
+import qualified Data.Text as T
+import qualified Data.ByteString.Lazy as BSL
+
+
 app :: Application
 app _ respond = do
     putStrLn "I've done some IO here"
@@ -29,9 +37,10 @@ index = responseFile
 app3 :: Application
 app3 request respond = respond $
   case rawPathInfo request of
-    "/"     -> index
-    "/raw/" -> plainIndex
-    _       -> notFound
+    "/"       -> index
+    "/raw/"   -> plainIndex
+    "/blaze/" -> blazeIndex
+    _         -> notFound
 
 plainIndex :: Response
 plainIndex = responseLBS
@@ -39,8 +48,27 @@ plainIndex = responseLBS
     [("Content-Type", "text/plain")]
     "Hello, PlainText"
 
+blazeIndex :: Response
+blazeIndex = responseLBS
+    status200
+    [("Content-Type", "text/html")]
+    numbersToText
+
 notFound :: Response
 notFound = responseLBS
     status404
     [("Content-Type", "text/plain")]
     "404 - Not Found"
+
+numbersToText :: BSL.ByteString
+numbersToText =
+  BHRU.renderHtml (numbers 5)
+
+--Blaze
+numbers :: Int -> H.Html
+numbers n = H.docTypeHtml $ do
+    H.head $ do
+        H.title "Natural numbers"
+    H.body $ do
+        H.p "A list of natural numbers:"
+        H.ul $ forM_ [1 .. n] (H.li . H.toHtml)
