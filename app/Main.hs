3a964952e230a996c3dc33431949381e7fa7a7fa
Eden <edendelgado@gmail.com>
2020-07-09 17:50:08 -0500

    created a thank you page
    Selecting a rank sends the user to thank you page, displaying the rank they chose.

--- app/Main.hs
+++ app/Main.hs
@@ -9,6 +9,9 @@ import qualified Text.Blaze.Html5 as H
 import qualified Text.Blaze.Html5.Attributes as A
 import qualified Text.Blaze.Html.Renderer.Utf8 as BHRU
 import qualified Data.ByteString.Lazy as BSL
+import qualified Data.ByteString as BS
+import Data.ByteString.Char8 (unpack)
+import Network.Wai.Parse (parseRequestBody)
 
 main :: IO ()
 main = do
@@ -17,17 +20,23 @@ main = do
 
 app :: Application
 app request respond = do
-  print $ rawQueryString request
-  print $ queryString request
-  print $ requestBodyLength request
-  params <- parseRequestBodyEx defaultParseRequestBodyOptions lbsBackEnd request
-  print params
+  --print $ rawQueryString request
+  --print $ queryString request
+  --print $ requestBodyLength request
+  --params <- parseRequestBodyEx defaultParseRequestBodyOptions lbsBackEnd request
+  --print params
+  parsedBody <- parseRequestBody lbsBackEnd request
+  case parsedBody of
+    (listOfParm,_) ->
+      print listOfParm
+    _ -> print "cool"
 
   respond $
     case rawPathInfo request of
       "/"           -> index
       "/plainIndex" -> plainIndex
-      "/about"     -> aboutUs
+      "/about"      -> aboutUs
+      "/ranked"     -> (thankYouRes $ fst parsedBody)
       _             -> notFound
 
 index :: Response
@@ -54,6 +63,18 @@ aboutUs = responseLBS
     [("Content-Type", "text/html")]
     (BHRU.renderHtml aboutUsHtml)
 
+thankYouRes :: [Param] -> Response
+thankYouRes params = do
+  let mbRankValue = lookup "rankSelect" params
+  case mbRankValue of
+    Nothing ->
+      notFound
+    Just rankValue ->
+      responseLBS
+        status200
+        [("Content-Type", "text/html")]
+        (BHRU.renderHtml $ thanksForRankHtml $ unpack rankValue)
+
 aboutUsHtml :: H.Html
 aboutUsHtml = H.docTypeHtml $ do
     H.head $ do
@@ -61,6 +82,14 @@ aboutUsHtml = H.docTypeHtml $ do
     H.body $ do
         H.p "About Us Page"
         H.p "trying to provide information"
+
+thanksForRankHtml :: String -> H.Html
+thanksForRankHtml rankValue = H.docTypeHtml $ do
+  H.head $ do
+    H.title "Thank You"
+  H.body $ do
+    H.p "Thank you for Rank"
+    H.p $ H.toHtml $ "Your ranked it a " <> rankValue
 
 libraryView :: HaskellLanguage -> H.Html
 libraryView hl = H.docTypeHtml $ do
@@ -78,7 +107,7 @@ rankSelect :: H.Html
 rankSelect =
   H.body $ do
     H.label $ H.toHtml ("Rank this page: "::String)
-    H.form H.! A.action "/about" H.! A.method "post" $ do
+    H.form H.! A.action "/ranked" H.! A.method "post" $ do
       H.select H.! A.name "rankSelect" $ do
         H.option H.! A.value "5" $ H.toHtml ("5 star"::String)
         H.option H.! A.value "4" $ H.toHtml ("4 star"::String)
