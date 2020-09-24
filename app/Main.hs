768d5447fae509f60d6b43b27ad17e4330e0f3fe
Eden <edendelgado@gmail.com>
2020-09-24 14:12:43 -0500

    Finished move database interactions to a dedicated file

--- app/Main.hs
+++ app/Main.hs
@@ -123,9 +123,7 @@ rankPath orvilleEnv request respond = do
 
   case (validRank mbRankValue, validExtension mbLEValue) of
     (Just rankValue, Just langExt) -> do
-      _ <- O.runOrville
-             (O.insertRecord rankTable $ RankRecord () rankValue)
-             orvilleEnv
+      _ <- insertNewRecord orvilleEnv (RankRecord () rankValue)
       _ <- checkAndUpdateTotalRank
              langExt
              rankValue
@@ -133,55 +131,6 @@ rankPath orvilleEnv request respond = do
       respond (thankYouRes $ (show $ rankInt rankValue))
     (_, _) ->
       indexPath orvilleEnv respond (Just errorMessage)
-
-checkAndUpdateTotalRank :: ExtensionId
-                        -> Rank
-                        -> O.OrvilleEnv Postgres.Connection
-                        -> IO ()
-checkAndUpdateTotalRank extenId (Rank newRankInt) orvilleEnv = do
-  mbRankTotalRecord <- findRankTotalRecord extenId orvilleEnv
-  case mbRankTotalRecord of
-    [] -> do
-      putStrLn "No record found"
-      _ <- O.runOrville
-             (O.insertRecord rankTotalTable $
-                RankTotalRecord
-                  { rankTotalId = ()
-                  , extensionId = extenId
-                  , rankCount   = 1
-                  , rankSum     = RankSum newRankInt
-                  }
-             )
-             orvilleEnv
-      pure ()
-    [rankTotalRecord] -> do
-      let
-        oldRankSum = (rankSumInt $ rankSum rankTotalRecord)
-      O.runOrville
-        (O.updateRecord
-          rankTotalTable
-          (rankTotalId rankTotalRecord)
-          (rankTotalRecord { rankTotalId = ()
-                           , rankCount   = (rankCount rankTotalRecord + 1)
-                           , rankSum     = (RankSum $ newRankInt + oldRankSum)
-                           }
-          )
-        )
-        orvilleEnv
-    _ -> putStrLn "Error multiple records found"
-
-
-
-findRankTotalRecord :: ExtensionId
-                    -> O.OrvilleEnv Postgres.Connection
-                    -> IO [RankTotalRecord RankTotalId]
-findRankTotalRecord eId orvilleEnv = do
-  O.runOrville
-    ( O.selectAll
-      rankTotalTable
-      (O.where_ (extensionIdField O..== eId))
-    )
-    orvilleEnv
 
 index :: Float -> Response
 index rankAvg = responseLBS
--- app/RunOnOrville.hs
+++ app/RunOnOrville.hs
@@ -4,8 +4,60 @@ import qualified Database.HDBC.PostgreSQL as Postgres
 import qualified Database.Orville.PostgreSQL as O
 
 import           Schema
+import           Types
 
 runMigrations :: O.OrvilleEnv Postgres.Connection -> IO ()
 runMigrations env =
   O.runOrville (O.migrateSchema allSchemas) env
+
+insertNewRecord :: O.OrvilleEnv Postgres.Connection -> RankRecord () -> IO (RankRecord RankId)
+insertNewRecord env rankValue =
+  O.runOrville (O.insertRecord rankTable $ rankValue) env
+
+checkAndUpdateTotalRank :: ExtensionId
+                        -> Rank
+                        -> O.OrvilleEnv Postgres.Connection
+                        -> IO ()
+checkAndUpdateTotalRank extenId (Rank newRankInt) orvilleEnv = do
+  mbRankTotalRecord <- findRankTotalRecord extenId orvilleEnv
+  case mbRankTotalRecord of
+    [] -> do
+      putStrLn "No record found"
+      _ <- O.runOrville
+             (O.insertRecord rankTotalTable $
+                RankTotalRecord
+                  { rankTotalId = ()
+                  , extensionId = extenId
+                  , rankCount   = 1
+                  , rankSum     = RankSum newRankInt
+                  }
+             )
+             orvilleEnv
+      pure ()
+    [rankTotalRecord] -> do
+      let
+        oldRankSum = (rankSumInt $ rankSum rankTotalRecord)
+      O.runOrville
+        (O.updateRecord
+          rankTotalTable
+          (rankTotalId rankTotalRecord)
+          (rankTotalRecord { rankTotalId = ()
+                           , rankCount   = (rankCount rankTotalRecord + 1)
+                           , rankSum     = (RankSum $ newRankInt + oldRankSum)
+                           }
+          )
+        )
+        orvilleEnv
+    _ -> putStrLn "Error multiple records found"
+
+findRankTotalRecord :: ExtensionId
+                    -> O.OrvilleEnv Postgres.Connection
+                    -> IO [RankTotalRecord RankTotalId]
+findRankTotalRecord eId orvilleEnv = do
+  O.runOrville
+    ( O.selectAll
+      rankTotalTable
+      (O.where_ (extensionIdField O..== eId))
+    )
+    orvilleEnv
 
