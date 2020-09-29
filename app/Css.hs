48aee8a3820016788c5f13866feaddfd6381d790
Eden <edendelgado@gmail.com>
2020-09-29 15:08:56 -0500

    Added package Clay
    Used Clay to create new CSS format to use on all HTML pages

--- app/Css.hs
+++ app/Css.hs
@@ -3,17 +3,31 @@ module Css where
 
 import           Network.Wai
 import           Network.HTTP.Types
+import           Clay
+import           Data.Text.Lazy.Encoding
 
 mainCssPath :: Response
 mainCssPath = responseLBS
     status200
     [("Content-Type", "text/css")]
-    " h2 {font-size: 34px;} \
-    \ \
-    \ h3 {font-size: 28px}\
-    \ \
-    \ h2, h3 { font-family: inherit ;\
-    \ margin-top: 22px; margin-bottom: 11px; font-weight: 500; line-height: 1.1; color: inherit }\
-    \ .Extension { padding-left: 15px; padding-right: 15px }\
-    \ "
+    (encodeUtf8 (render mainCss))
 
+mainCss :: Css
+mainCss = do
+  let
+    fontSet   = fontFamily ["Raleway", "Helvetica", "Arial"] [monospace]
+    marginSet = do
+      marginTop $ px 22
+      marginBottom $ px 11
+
+  h2 ? do
+    (fontSize $ px 34 )
+    fontSet
+    marginSet
+  h3 ? do
+    (fontSize $ px 28 )
+    fontSet
+    marginSet
+  ".Extension" ? do
+      (paddingRight $ px 15)
+      (paddingLeft $ px 15)
